5a048a078871beec75b375fe49338e2344c508ab
vaisnava <42044316+vaisnava@users.noreply.github.com>
2020-02-20 19:35:37 -0600

    Second commit with new class chant2

--- src/mantra/Chant1.java
+++ src/mantra/Chant1.java
@@ -4,6 +4,9 @@ public class Chant1 {
 
 	public static void main(String[] args) {
 	System.out.println("this is hare krishna mantra");
+	
+	Chant2 ch = new Chant2();
+	     ch.om();
 	}
 
 }
---
+++ src/mantra/Chant2.java
@@ -0,0 +1,11 @@
+package mantra;
+
+public class Chant2 {
+
+	
+		public void om() {
+			System.out.println("Hare Krishna Hare Krishna");
+
+	}
+
+}
